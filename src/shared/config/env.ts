37082473f6bf245c7c56f0b67a325f9f72c0637d
Bahodir0807 <sultonovbahodir00@gmail.com>
2026-04-04 22:03:12 +0500

    update

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -1,4 +1,4 @@
-const fallbackApiUrl = 'http://b.sultonoway.uz';
+const fallbackApiUrl = 'https://b.sultonoway.uz';
 
 export const env = {
   apiUrl: (import.meta.env.VITE_API_URL as string | undefined) ?? fallbackApiUrl,
